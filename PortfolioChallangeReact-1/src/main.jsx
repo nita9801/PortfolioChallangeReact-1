239e0790edfb5cf9bc397d014fc1b6a0ff9c1718
Christine Lopez <mrschristinemlopez@gmail.com>
2025-02-17 20:17:04 -0500

    added some new installs for icons and animation

--- PortfolioChallangeReact-1/src/main.jsx
+++ PortfolioChallangeReact-1/src/main.jsx
@@ -2,9 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import React from 'react';
+import Header from './components/Header';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+function App() {
+  return (
+    <div>
+      <Header />
+      <About />
+      <Portfolio />
+      <Contact />
+      <Resume />
+    </div>
+  );
+}
+
+export default App;
